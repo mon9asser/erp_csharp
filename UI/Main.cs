52c56ccc0f6f49f228855df8c650420aa01f5681
Montasser Mossallem <moun2030@gmail.com>
2022-01-07 21:08:16 +0300

    Main Form

--- UI/Main.cs
+++ UI/Main.cs
@@ -12,6 +12,29 @@ namespace sales_management.UI
 {
     public partial class Main : Form
     {
+        
+        private int userId;
+        private string userName;
+        private string fullName;
+
+        public void setUserInfo( int userId, string userName, string fullName ) {
+            this.userId = userId;
+            this.userName = userName;
+            this.fullName = fullName;
+        }
+
+        public string[] getUserInfo() {
+
+            string[] userInfo = new string[3];
+
+            userInfo[0] = this.userId.ToString();
+            userInfo[1] = this.userName;
+            userInfo[2] = this.fullName;
+
+            return userInfo;
+        }
+
+
         public Main()
         {
             InitializeComponent();
