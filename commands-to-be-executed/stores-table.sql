4ab604e209b5542f48530f25a4994636ce615b39
Montasser <moun2030@gmail.com>
2022-05-12 21:08:23 +0300

    updates

--- commands-to-be-executed/stores-table.sql
+++ commands-to-be-executed/stores-table.sql
@@ -10,7 +10,16 @@ total_price
 type ==> 0 => decrement 1 => 
 */
 
-
+CREATE TYPE [dbo].[journal_details] AS TABLE(
+	[journal_id] [int] NULL,
+	[debit] decimal(18,2) NULL,
+	[credit] decimal(18,2) NULL,
+	[description] [text] NULL,
+	[cost_center_number] [varchar](50) NULL,
+	[date] [datetime] NULL,
+	[account_number] [varchar](50) NULL
+)
+GO
 -----------------------------------
 ------ تقرير المسحوبات عن الفترة
 -- كميات البيع
@@ -54,4 +63,47 @@ delete from withdraw_document where id = @cid;
 delete from document_details where doc_id = @cid and doc_type = 6;
 delete from journals where id=@jid;
 delete from journal_details where journal_id = @jid;
+
+---=========================================================
+
+WITH CTE AS
+(
+SELECT t2.TransDate, 
+       t2.Credit, 
+       t2.Debit, 
+       SUM(COALESCE(t1.credit, 0) - COALESCE(t1.debit, 0)) AS Balance
+FROM Test t1 
+INNER JOIN Test t2
+    ON t1.TransDate <= t2.TransDate
+
+GROUP BY t2.TransDate, t2.Credit, t2.Debit
+)
+SELECT * 
+FROM CTE
+WHERE (TransDate >= '2014/01/11' AND TransDate <= '2014/02/28' ) 
+
+-------------------------------------------------
+
+SELECT *, SUM(COALESCE(CAST(credit AS DECIMAL(10, 2)),0) - COALESCE(CAST(debit AS DECIMAL(10, 2)),0)) over (order by id) as Balance
+FROM journal_details  
+INNER JOIN journals ON journal_details.journal_id = journals.id
+where journal_details.account_number = 2106;
+
+
+-------------------------------------------------------
+WITH CTE AS
+(
+	SELECT t2.TransDate, 
+		   t2.Credit, 
+		   t2.Debit, 
+		   SUM(COALESCE(t1.credit, 0) - COALESCE(t1.debit, 0)) AS Balance
+	FROM Test t1 
+	INNER JOIN Test t2
+		ON t1.TransDate <= t2.TransDate
+
+	GROUP BY t2.TransDate, t2.Credit, t2.Debit
+)
+SELECT * 
+FROM CTE
+WHERE (TransDate >= '2014/01/11' AND TransDate <= '2014/02/28' )
 
