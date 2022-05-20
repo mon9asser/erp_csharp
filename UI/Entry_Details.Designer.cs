b3076769f3796e1b2a4e0e85cd7976a35bb951cc
Montasser <moun2030@gmail.com>
2022-05-21 01:41:03 +0300

    updates

---
+++ UI/Entry_Details.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace sales_management.UI
+{
+    partial class _ُEntry_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entry_number_field = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.entry_id = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.current_invoice_page = new System.Windows.Forms.Label();
+            this.add_new_button = new System.Windows.Forms.Button();
+            this.first_record_button = new System.Windows.Forms.Button();
+            this.last_record_button = new System.Windows.Forms.Button();
+            this.next_button = new System.Windows.Forms.Button();
+            this.previous_button = new System.Windows.Forms.Button();
+            this.save_button = new System.Windows.Forms.Button();
+            this.edit_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // entry_number_field
+            // 
+            this.entry_number_field.Location = new System.Drawing.Point(87, 23);
+            this.entry_number_field.Name = "entry_number_field";
+            this.entry_number_field.Size = new System.Drawing.Size(211, 20);
+            this.entry_number_field.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "رقم القيد :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // entry_id
+            // 
+            this.entry_id.Location = new System.Drawing.Point(304, 23);
+            this.entry_id.Name = "entry_id";
+            this.entry_id.Size = new System.Drawing.Size(21, 20);
+            this.entry_id.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "البيان :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(387, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 62);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "التاريخ :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(87, 65);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(211, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(768, 294);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // current_invoice_page
+            // 
+            this.current_invoice_page.AutoSize = true;
+            this.current_invoice_page.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.current_invoice_page.Location = new System.Drawing.Point(401, 428);
+            this.current_invoice_page.Name = "current_invoice_page";
+            this.current_invoice_page.Size = new System.Drawing.Size(82, 27);
+            this.current_invoice_page.TabIndex = 145;
+            this.current_invoice_page.Text = "00 / 00";
+            this.current_invoice_page.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // add_new_button
+            // 
+            this.add_new_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_new_button.Image = global::sales_management.Properties.Resources.add_new;
+            this.add_new_button.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.add_new_button.Location = new System.Drawing.Point(14, 423);
+            this.add_new_button.Name = "add_new_button";
+            this.add_new_button.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.add_new_button.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.add_new_button.Size = new System.Drawing.Size(133, 39);
+            this.add_new_button.TabIndex = 146;
+            this.add_new_button.Text = "إضافة  جديدة";
+            this.add_new_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.add_new_button.UseVisualStyleBackColor = true;
+            // 
+            // first_record_button
+            // 
+            this.first_record_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.first_record_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.first_record_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.first_record_button.Image = global::sales_management.Properties.Resources.last_btn;
+            this.first_record_button.Location = new System.Drawing.Point(281, 423);
+            this.first_record_button.Name = "first_record_button";
+            this.first_record_button.Size = new System.Drawing.Size(50, 38);
+            this.first_record_button.TabIndex = 144;
+            this.first_record_button.UseVisualStyleBackColor = false;
+            // 
+            // last_record_button
+            // 
+            this.last_record_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.last_record_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.last_record_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.last_record_button.Image = global::sales_management.Properties.Resources.first_btn;
+            this.last_record_button.Location = new System.Drawing.Point(551, 423);
+            this.last_record_button.Name = "last_record_button";
+            this.last_record_button.Size = new System.Drawing.Size(50, 38);
+            this.last_record_button.TabIndex = 143;
+            this.last_record_button.UseVisualStyleBackColor = false;
+            // 
+            // next_button
+            // 
+            this.next_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.next_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next_button.Image = global::sales_management.Properties.Resources.next_btn;
+            this.next_button.Location = new System.Drawing.Point(337, 423);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(50, 38);
+            this.next_button.TabIndex = 142;
+            this.next_button.UseVisualStyleBackColor = false;
+            // 
+            // previous_button
+            // 
+            this.previous_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.previous_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous_button.Image = global::sales_management.Properties.Resources.prev_btn;
+            this.previous_button.Location = new System.Drawing.Point(495, 423);
+            this.previous_button.Name = "previous_button";
+            this.previous_button.Size = new System.Drawing.Size(50, 38);
+            this.previous_button.TabIndex = 141;
+            this.previous_button.UseVisualStyleBackColor = false;
+            // 
+            // save_button
+            // 
+            this.save_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.save_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.save_button.Image = global::sales_management.Properties.Resources.save_data;
+            this.save_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.save_button.Location = new System.Drawing.Point(154, 423);
+            this.save_button.Name = "save_button";
+            this.save_button.Padding = new System.Windows.Forms.Padding(20, 0, 5, 0);
+            this.save_button.Size = new System.Drawing.Size(98, 38);
+            this.save_button.TabIndex = 140;
+            this.save_button.Text = " حفظ";
+            this.save_button.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.save_button.UseVisualStyleBackColor = true;
+            // 
+            // edit_button
+            // 
+            this.edit_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.edit_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.edit_button.Image = global::sales_management.Properties.Resources.icons8_update_20;
+            this.edit_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.edit_button.Location = new System.Drawing.Point(626, 423);
+            this.edit_button.Name = "edit_button";
+            this.edit_button.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.edit_button.Size = new System.Drawing.Size(72, 39);
+            this.edit_button.TabIndex = 147;
+            this.edit_button.Text = "تعديل";
+            this.edit_button.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.edit_button.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::sales_management.Properties.Resources.icons8_update_20;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(704, 423);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(8, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(72, 39);
+            this.button1.TabIndex = 148;
+            this.button1.Text = "بحث";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // _ُEntry_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.edit_button);
+            this.Controls.Add(this.current_invoice_page);
+            this.Controls.Add(this.add_new_button);
+            this.Controls.Add(this.first_record_button);
+            this.Controls.Add(this.last_record_button);
+            this.Controls.Add(this.next_button);
+            this.Controls.Add(this.previous_button);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.entry_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.entry_number_field);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "_ُEntry_Details";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "قيود اليومية";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox entry_number_field;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox entry_id;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label current_invoice_page;
+        private System.Windows.Forms.Button add_new_button;
+        private System.Windows.Forms.Button first_record_button;
+        private System.Windows.Forms.Button last_record_button;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button previous_button;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.Button edit_button;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ UI/Entry_Details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sales_management.UI
+{
+    public partial class _ُEntry_Details : Form
+    {
+        public _ُEntry_Details()
+        {
+            InitializeComponent();
+        }
+    }
+}
