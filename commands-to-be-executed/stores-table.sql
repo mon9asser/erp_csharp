b71f1e8f6fc9beb1bc7a954b91864269058ce01d
Montasser <moun2030@gmail.com>
2022-05-27 21:10:08 +0300

    updates

--- commands-to-be-executed/stores-table.sql
+++ commands-to-be-executed/stores-table.sql
@@ -43,111 +43,148 @@ truncate table __purchase_invoice;
 truncate table ____purchase_invoice;
 truncate table ____invoice_sales; 
 
+ 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE PROC Profit_Statement_List
+
+AS 
+
+declare @date_from as datetime
+set @date_from = '2022-05-05 00:00:00.000';
+
+declare @date_to as datetime
+set @date_to = '2023-05-05 00:00:00.000';
+
+
+
+--==========================================================
+--			SALES NET TOTAL
+--========================================================== 
+
+DECLARE @net_total_sales AS DECIMAL; 
 
 -- SALES DATA
 DECLARE @total_sales AS DECIMAL
 SET @total_sales = (SELECT SUM(COALESCE(CAST(total_price AS DECIMAL(18,2)), 0 )) FROM document_details 
 	INNER JOIN invoice_sales ON document_details.doc_id = invoice_sales.id  
-WHERE is_out=  1 AND doc_type = 0);
+WHERE document_details.is_out=  1 AND document_details.doc_type = 0 and invoice_sales.[date] between @date_from and @date_to) ;
 --SELECT @total_sales AS sales_value;
- 
--- SALES COSTS 
-DECLARE @total_costs AS DECIMAL
-SET @total_costs = (SELECT SUM(COALESCE(CAST(total_cost AS DECIMAL(18,2)), 0 )) FROM document_details 
-	INNER JOIN invoice_sales ON document_details.doc_id = invoice_sales.id  
-WHERE is_out=  1 AND doc_type = 0);
---SELECT @total_costs AS total_cost;
 
+-- Commercial Discounts 
+DECLARE @commercial_discount AS DECIMAL
+SET @commercial_discount = (SELECT SUM(COALESCE(CAST(discount_name AS DECIMAL(18,2)), 0 )) FROM invoice_sales WHERE invoice_sales.[date] between @date_from and @date_to );
+--SELECT @commercial_discount AS commercial_discount;
+ 
 -- RETURN SALES DATA
 DECLARE @return_sales AS DECIMAL
 SET @return_sales = (SELECT SUM(COALESCE(CAST(total_price AS DECIMAL(18,2)), 0 )) FROM document_details 
 	INNER JOIN invoice_return_sales ON document_details.doc_id = invoice_return_sales.id  
-WHERE is_out= 0 AND doc_type = 2);
+WHERE document_details.is_out=  0 AND document_details.doc_type = 2 and invoice_return_sales.[date] between @date_from and @date_to) ;
 --SELECT @return_sales AS return_sales;
 
--- RETURN SALES COSTS
-DECLARE @return_sales_cost AS DECIMAL
-SET @return_sales_cost = (SELECT SUM(COALESCE(CAST(total_cost AS DECIMAL(18,2)), 0 )) FROM document_details 
-	INNER JOIN invoice_return_sales ON document_details.doc_id = invoice_return_sales.id  
-WHERE is_out= 0 AND doc_type = 2); 
 
--- Calculations 
-DECLARE @net_sale_data AS DECIMAL
-SET @net_sale_data = (SELECT @total_sales) - (SELECT @total_costs);
-
-
-DECLARE @net_return AS DECIMAL
-SET @net_return = (SELECT @return_sales) - (SELECT @return_sales_cost);
- 
-DECLARE @net_sales AS DECIMAL 
-SET @net_sales = ( SELECT @net_sale_data ) - ( SELECT @net_return);
-SELECT @net_sales;
-
-
--- MINIUS ALLOWED DISCOUNTS 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-==========================================================
-			SALES NET TOTAL
-==========================================================
-
--- SALES DATA
-DECLARE @total_sales AS DECIMAL
-SET @total_sales = (SELECT SUM(COALESCE(CAST(total_price AS DECIMAL(18,2)), 0 )) FROM document_details 
-	INNER JOIN invoice_sales ON document_details.doc_id = invoice_sales.id  
-WHERE is_out=  1 AND doc_type = 0);
---SELECT @total_sales AS sales_value;
-
--- RETURN SALES DATA
-DECLARE @return_sales AS DECIMAL
-SET @return_sales = (SELECT SUM(COALESCE(CAST(total_price AS DECIMAL(18,2)), 0 )) FROM document_details 
-	INNER JOIN invoice_return_sales ON document_details.doc_id = invoice_return_sales.id  
-WHERE is_out= 0 AND doc_type = 2);
---SELECT @return_sales AS return_sales;
+-- Commercial Discounts FOR RETURNS 
+DECLARE @return_commercial_discount AS DECIMAL
+SET @return_commercial_discount = (SELECT SUM(COALESCE(CAST(discount_name AS DECIMAL(18,2)), 0 )) FROM invoice_return_sales WHERE invoice_return_sales.[date] between @date_from and @date_to );
+--SELECT @return_commercial_discount AS commercial_discount;
 
 -- ALLOWED DISCOUNTS
 DECLARE @sales_allowed_discounts AS DECIMAL
-SET @sales_allowed_discounts = (select SUM(COALESCE(debit, 0)) from journal_details where account_number = '5104');
---SELECT @sales_allowed_discounts AS allowed_discounts;
+SET @sales_allowed_discounts = (select SUM(COALESCE(debit, 0)) from journal_details INNER JOIN journals ON journal_details.journal_id = journals.id where account_number = '5104' AND journals.updated_date between @date_from and @date_to);
 
 
-DECLARE @total_net_sales AS DECIMAL;
-SET @total_net_sales = ( SELECT @total_sales ) - ( ( SELECT @return_sales ) + ( SELECT @sales_allowed_discounts ) );
+SET @total_sales = ( ( SELECT @total_sales ) - ( SELECT @commercial_discount ) );
+SET @return_sales = ( ( SELECT @return_sales ) - ( SELECT @return_commercial_discount ) );
+DECLARE @total_net_sales AS DECIMAL ;
+SET @total_net_sales = (SELECT @total_sales) - (SELECT @return_sales);
 
--- SALES NET TOTAL 
+--==========================================================
+--			COST OF SELL GODS
+--========================================================== 
+
+-- COST OF SOLD GODS
+DECLARE @total_cost AS DECIMAL
+SET @total_cost = (SELECT SUM(COALESCE(CAST(total_cost AS DECIMAL(18,2)), 0 )) FROM document_details 
+	INNER JOIN invoice_sales ON document_details.doc_id = invoice_sales.id  
+WHERE document_details.is_out=  1 AND document_details.doc_type = 0 and invoice_sales.[date] between @date_from and @date_to) ;
+--SELECT @total_cost AS total_cost;
+
+
+-- RETURN SALES COST 
+DECLARE @return_sales_cost AS DECIMAL
+SET @return_sales_cost = (SELECT SUM(COALESCE(CAST(total_cost AS DECIMAL(18,2)), 0 )) FROM document_details 
+	INNER JOIN invoice_return_sales ON document_details.doc_id = invoice_return_sales.id  
+WHERE document_details.is_out= 0 AND document_details.doc_type = 2 and invoice_return_sales.[date] between @date_from and @date_to) ;
+--SELECT @return_sales_cost AS return_total_cost;
+
+
+-- ALLOWED DISCOUNTS
+DECLARE @cost_allowed_discounts AS DECIMAL
+SET @cost_allowed_discounts = (select SUM(CAST(COALESCE(credit, 0) AS decimal(18,2))) from journal_details INNER JOIN journals ON journal_details.journal_id = journals.id where account_number = '4103' AND journals.updated_date between @date_from and @date_to);
+--select @cost_allowed_discounts;
+
+-- PURCHASE EXPENSES
+DECLARE @purchase_expenses AS DECIMAL
+SET @purchase_expenses = (select SUM(CAST(COALESCE(credit, 0) AS decimal(18,2)) - CAST(COALESCE(debit, 0) AS decimal(18,2))) from journal_details INNER JOIN journals ON journal_details.journal_id = journals.id where account_number = '5102' AND journals.updated_date between @date_from and @date_to);
+--select @purchase_expenses;
+IF @return_sales_cost IS NULL 
+SET @return_sales_cost = 0;
+
+IF @cost_allowed_discounts IS NULL 
+SET @cost_allowed_discounts = 0;
+
+IF @purchase_expenses IS NULL 
+SET @purchase_expenses = 0;
+
+SET @total_cost = ( SELECT @total_cost ) - ( SELECT @return_sales_cost );
+SET @total_cost = ( SELECT @total_cost ) - ( SELECT @cost_allowed_discounts );
+SET @total_cost = ( SELECT @total_cost ) + ( SELECT @purchase_expenses );
+
+
+
+--==========================================================
+--			OTHER REVENUES
+--========================================================== 
+DECLARE @other_revenuse AS DECIMAL;
+SET @other_revenuse = (SELECT SUM(CAST(COALESCE(credit, 0) AS decimal(18,2)) - CAST(COALESCE(debit, 0) AS decimal(18,2))) FROM journal_details 
+INNER JOIN journals ON journal_details.journal_id = journals.id 
+WHERE journals.updated_date BETWEEN @date_from AND @date_to AND account_number LIKE '420%'); 
+
+--==========================================================
+--			NEEDED GIVENS
+--========================================================== 
 SELECT @total_net_sales AS total_net_sales;
-
-
-==========================================================
-			COST NET TOTAL
-==========================================================
+SELECT @total_cost AS total_cost;
+SELECT @other_revenuse AS other_revenues;
