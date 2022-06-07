8cf289a9518114613763ba901cc5ba5d2261bbeb
Montasser <moun2030@gmail.com>
2022-06-07 21:27:01 +0300

    updates

---
+++ PL/Users.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace sales_management.PL
+{
+    class Users
+    {
+
+        /*
+            =====================================================================
+            USERS DATA
+            =====================================================================
+        */
+        public void Update( string fname, string lname, string password, int responsability, int userId = -1)
+        {
+            DB.DataAccessLayer DAL = new DB.DataAccessLayer();
+
+            SqlParameter[] param = new SqlParameter[5];
+
+            param[0] = new SqlParameter("@id", SqlDbType.Int);
+            param[0].Value = userId;
+
+            param[1] = new SqlParameter("@first_name", SqlDbType.VarChar);
+            param[1].Value = fname;
+
+            param[2] = new SqlParameter("@last_name", SqlDbType.VarChar);
+            param[2].Value = lname;
+
+            param[3] = new SqlParameter("@password", SqlDbType.VarChar);
+            param[3].Value = password;
+
+            param[4] = new SqlParameter("@responsability_id", SqlDbType.Int);
+            param[4].Value = responsability;
+             
+            DAL.Open();
+            DAL.ExecuteCommand("Update_User_Data", param);
+            DAL.Close();
+
+        }
+
+        public DataTable Get_All_Users() {
+
+            DataTable tble;
+
+            DB.DataAccessLayer DAL = new DB.DataAccessLayer();
+
+            DAL.Open();
+            tble = DAL.SelectData("Get_All_User_Data", null);
+            DAL.Close();
+
+            return tble;
+
+        }
+
+        public void Delete( int userId = -1)
+        {
+            DB.DataAccessLayer DAL = new DB.DataAccessLayer();
+
+            SqlParameter[] param = new SqlParameter[1];
+
+            param[0] = new SqlParameter("@id", SqlDbType.Int);
+            param[0].Value = userId; 
+
+            DAL.Open();
+            DAL.ExecuteCommand("Delete_User_Data", param);
+            DAL.Close();
+
+        }
+
+
+        /*
+           =====================================================================
+           PRIVILEGE AND RESP DATA
+           =====================================================================
+       */
+        public void Delete_Responsability_With_Privileges(int resp_id = -1)
+        {
+            DB.DataAccessLayer DAL = new DB.DataAccessLayer();
+
+            SqlParameter[] param = new SqlParameter[1];
+
+            param[0] = new SqlParameter("@id", SqlDbType.Int);
+            param[0].Value = resp_id;
+
+            DAL.Open();
+            DAL.ExecuteCommand("Delete_Responsability_With_Privileges", param);
+            DAL.Close();
+
+        }
+
+    }
+}
---
+++ UI/employees.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace sales_management.UI
+{
+    partial class employees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.next_button = new System.Windows.Forms.Button();
+            this.previous_button = new System.Windows.Forms.Button();
+            this.first_record_button = new System.Windows.Forms.Button();
+            this.last_record_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "الإسم الأول :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "الإسم الثاني :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "الرقم السري :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "الصلاحية :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(117, 218);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(117, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(250, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(9, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 38);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "حفظ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(313, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 38);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "حذف";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // next_button
+            // 
+            this.next_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.next_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next_button.Image = global::sales_management.Properties.Resources.next_btn;
+            this.next_button.Location = new System.Drawing.Point(145, 273);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(50, 38);
+            this.next_button.TabIndex = 134;
+            this.next_button.UseVisualStyleBackColor = false;
+            // 
+            // previous_button
+            // 
+            this.previous_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.previous_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous_button.Image = global::sales_management.Properties.Resources.prev_btn;
+            this.previous_button.Location = new System.Drawing.Point(201, 273);
+            this.previous_button.Name = "previous_button";
+            this.previous_button.Size = new System.Drawing.Size(50, 38);
+            this.previous_button.TabIndex = 133;
+            this.previous_button.UseVisualStyleBackColor = false;
+            // 
+            // first_record_button
+            // 
+            this.first_record_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.first_record_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.first_record_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.first_record_button.Image = global::sales_management.Properties.Resources.last_btn;
+            this.first_record_button.Location = new System.Drawing.Point(89, 273);
+            this.first_record_button.Name = "first_record_button";
+            this.first_record_button.Size = new System.Drawing.Size(50, 38);
+            this.first_record_button.TabIndex = 136;
+            this.first_record_button.UseVisualStyleBackColor = false;
+            // 
+            // last_record_button
+            // 
+            this.last_record_button.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.last_record_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.last_record_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.last_record_button.Image = global::sales_management.Properties.Resources.first_btn;
+            this.last_record_button.Location = new System.Drawing.Point(255, 273);
+            this.last_record_button.Name = "last_record_button";
+            this.last_record_button.Size = new System.Drawing.Size(50, 38);
+            this.last_record_button.TabIndex = 135;
+            this.last_record_button.UseVisualStyleBackColor = false;
+            // 
+            // employees
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 325);
+            this.Controls.Add(this.first_record_button);
+            this.Controls.Add(this.last_record_button);
+            this.Controls.Add(this.next_button);
+            this.Controls.Add(this.previous_button);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "employees";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "الموظفين";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button previous_button;
+        private System.Windows.Forms.Button first_record_button;
+        private System.Windows.Forms.Button last_record_button;
+    }
+}
